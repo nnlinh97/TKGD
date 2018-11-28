cd8e0737682a60fbde7af1813280d8238b540c68
TRANDINHHUAN <dinhhuantran3010@gmail.com>
2018-11-28 20:29:55 +0700

    Sua slider gia tien trong shop

--- html/js/main.js
+++ html/js/main.js
@@ -556,14 +556,14 @@ NOTE: main.js, All custom script and plugin activation script in this file.
     $("#slider-range").slider({
         range: true,
         min: 0,
-        max: 100,
-        values: [0, 85],
+        max: 500000,
+        values: [0, 100000],
     slide: function( event, ui ) {
-        $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+        $( "#amount" ).val(ui.values[ 0 ] + "vnđ" + " - " + ui.values[ 1 ] +" vnđ" );
       }
     });
-    $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
-      " - $" + $( "#slider-range" ).slider( "values", 1 ) );
+    $( "#amount" ).val( $( "#slider-range" ).slider( "values", 0 ) +
+      " - " + $( "#slider-range" ).slider( "values", 1 ) + "vnđ");
 
 
 
